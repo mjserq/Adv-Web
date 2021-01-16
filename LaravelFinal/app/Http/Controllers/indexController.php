6367fe6032b079d7c7eaf9d77f12ac939b158c9c
mjserq <mjserquina@yahoo.com>
2021-01-16 21:55:38 +0800

    Project Added

---
+++ LaravelFinal/app/Http/Controllers/indexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class indexController extends Controller
+{
+    
+    public function index(){
+    	return view('index');
+    }
+    public function generic(){
+    	return view('generic');
+    }
+    public function elements(){
+    	return view('elements');
+    }
+}

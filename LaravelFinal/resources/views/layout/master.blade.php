b10249a62cd81dc6c17b68d63e4c1ad38a3e8095
mjserq <mjserquina@yahoo.com>
2021-01-16 23:30:36 +0800

    Update master.blade.php

--- LaravelFinal/resources/views/layout/master.blade.php
+++ LaravelFinal/resources/views/layout/master.blade.php
@@ -13,7 +13,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="/assets/css/main.css" />
 	</head>
-
+	<body>
 
 
 		<!-- Header -->
